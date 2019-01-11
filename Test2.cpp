65c59900744becbc118da4f79ab74c8f8ec3e137
Kishor Vijay Patil <paatil.kishor@gmail.com>
2019-01-11 15:53:14 +0530

    Added Test2 Class

--- Test2.cpp
+++ Test2.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
 
-
+class Test2
+{
+  public:
+  Test2()
+  {
+    cout<<"No Arg Constructor"<<endl;
+  }
+  
+  ~Test2()
+  {
+    cout<<"Destructor"<<endl;
+  }
+};
 int main()
 {
   cout<<"Hello World"<<endl;    // 1st Commit
